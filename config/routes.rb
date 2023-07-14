5cbeeefd90e564557e62526d3df94113eb16e6f2
Paul Rojas <paulrojasc@gmail.com>
2023-07-14 16:04:04 -0400

    Se agregan ultimos detalles visuales e index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/index'
   resources :movies
   resources :clients
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#index"
 end
